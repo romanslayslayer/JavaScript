94797ded8aa0c985601c2b6e3faadc460f4fdf69
Roman <razinka77@gmail.com>
2018-12-13 12:13:54 +0200

    Это наш первый комит

---
+++ lesson_4/script4.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = "Я запустился !!!!";
+const pi = 3.14;
+console.log(second);
+//document.write(second);
+
+let persone = {
+    name :"Serg",
+    age : 25,
+    id : 345267,
+    nasui : true
+};
+console.log(persone.id + " <----номер моей машины");
+
+let arr =['ford','nissan','suzuki'];
+console.log(arr[1]+" <---- Это моя машина");
+let sam;
+
+console.log(4/0);
+console.log(sam);
+console.log(persone.age);
+var obj = {};
+console.log(arr[1]);
---
+++ lesson_5/script5.js
@@ -0,0 +1,9 @@
+//alert("Hello Word !!!");
+//let arr = ['toyota','nissan','gepard'];
+//let answer = confirm("Are you here???");
+//console.log(answer);
+//let answert = prompt("Есть ли Вам 18 ???","Да");
+//console.log(answert);
+//console.log(typeof(answert));
+//console.log(typeof(arr));
+//console.log(typeof(null));
